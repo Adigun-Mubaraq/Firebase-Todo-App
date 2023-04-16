1709e08a5c6a1f46bc2a64d8e352f043f678c29f
Adigun-Mubaraq <adigun.mubaraq06@gmail.com>
2023-04-16 22:02:14 +0100

    module dotenv

---
+++ types/env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+    export const apiKey: "AIzaSyDdzugwUFPW39cV5A-wNUWeBw2g-oRTTUc",
+    export const authDomain: "rntodoapp-e6a70.firebaseapp.com",
+    export const projectId: "rntodoapp-e6a70",
+    export const storageBucket: "rntodoapp-e6a70.appspot.com",
+    export const messagingSenderId: "963298885164",
+    export const appId: "1:963298885164:web:647df95df995e84b215b89"
+}
