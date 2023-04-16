2068075d867409ac7bb602d34cc8fd727689cfd8
Adigun-Mubaraq <adigun.mubaraq06@gmail.com>
2023-04-16 11:27:11 +0100

    Adding Data to the firestore

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,12 +1,71 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { FIRESTORE_DB } from "../../FirebaseConfig";
+
+export interface Todo {
+  done: boolean;
+  id: string;
+  title: string;
+}
 
 const List = () => {
+  const [todos, setTodos] = useState<any[]>([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = async () => {
+    try {
+      const docRef = await addDoc(collection(FIRESTORE_DB, "todos"), {
+        title: todo,
+        done: false,
+      });
+      setTodo("");
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add new todo"
+            onChangeText={(text: string) => setTodo(text)}
+            value={todo}
+          />
+          <Button onPress={addTodo} title="Add Todo" disabled={todo === ""} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  form: {
+    marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
 
 export default List;
