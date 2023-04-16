961e190c886f84e1ff4bc53b622b167c98cb8a1f
Adigun-Mubaraq <adigun.mubaraq06@gmail.com>
2023-04-16 11:55:32 +0100

    Implement flatlist component for rendering Todo List

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -4,9 +4,13 @@ import {
   TextInput,
   Button,
   SafeAreaView,
+  TouchableOpacity,
+  Text,
+  FlatList
 } from "react-native";
-import React, { useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { FIRESTORE_DB } from "../../FirebaseConfig";
 
 export interface Todo {
@@ -19,6 +23,27 @@ const List = () => {
   const [todos, setTodos] = useState<any[]>([]);
   const [todo, setTodo] = useState("");
 
+  useEffect(() => {
+    const todoRef = collection(FIRESTORE_DB, "todos");
+
+    const subscriber = onSnapshot(todoRef, {
+      next: (snapshot) => {
+        const todos: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          todos.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+
+        setTodos(todos);
+      },
+    });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
   const addTodo = async () => {
     try {
       const docRef = await addDoc(collection(FIRESTORE_DB, "todos"), {
@@ -30,6 +55,36 @@ const List = () => {
     } catch (e) {
       console.error("Error adding document: ", e);
     }
+  };
+
+  const renderTodo = ({ item }: any) => {
+    const ref = doc(FIRESTORE_DB, `todos/${item.id}`);
+
+    const toggleDone = async () => {
+      updateDoc(ref, { done: !item.done });
+    };
+
+    const deleteItem = async () => {
+      deleteDoc(ref);
+    };
+
+    return (
+      <View style={styles.todoContainer}>
+        <TouchableOpacity onPress={toggleDone} style={styles.todo}>
+          {item.done && (
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          )}
+          {!item.done && <Entypo name="circle" size={32} color="black" />}
+          <Text style={styles.todoText}>{item.title}</Text>
+        </TouchableOpacity>
+        <Ionicons
+          name="trash-bin-outline"
+          size={24}
+          color="red"
+          onPress={deleteItem}
+        />
+      </View>
+    );
   };
 
   return (
@@ -45,6 +100,16 @@ const List = () => {
           <Button onPress={addTodo} title="Add Todo" disabled={todo === ""} />
         </View>
       </View>
+      {todos.length > 0 && (
+        <View>
+          <FlatList
+            data={todos}
+            renderItem={renderTodo}
+            keyExtractor={(todo) => todo.id}
+            // removeClippedSubviews={true}
+          />
+        </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -65,6 +130,22 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 10,
     backgroundColor: "#fff",
+  },
+  todo: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  },
+  todoText: {
+    flex: 1,
+    paddingHorizontal: 4,
+  },
+  todoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginVertical: 4,
   },
 });
 
